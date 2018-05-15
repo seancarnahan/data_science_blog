cca9b8e9091c335714e7500c73b1941f15ea0886
Sean Carnahan <seancarnahan@campus-024-183.ucdavis.edu>
2018-05-15 15:39:37 -0700

    added some structuring ideas to setup S3 calls

--- src/components/js/Home.jsx
+++ src/components/js/Home.jsx
@@ -3,12 +3,16 @@ import {Link} from 'react-router-dom'; //from react-router library
 import {Jumbotron, Grid, Button} from 'react-bootstrap';
 import './css/Home.css';
 
-export default class Home extends Component {
+//this method should make the ajax call and bring down and display at least 5 posts
+
+class Home extends Component {
   render(){
     return (
       <div>
-        
+
       </div>
     );
   }
 }
+
+export default Home;
